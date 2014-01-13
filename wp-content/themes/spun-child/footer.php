3ea8ca5a00615aad89d4a8d628e2eb211630eb3f
swhustla <fdkelly@gmail.com>
2014-01-13 21:54:40 +0000

    font size and footer fix

--- wp-content/themes/spun-child/footer.php
+++ wp-content/themes/spun-child/footer.php
@@ -15,7 +15,7 @@
 		<div class="site-info">
 			<?php do_action( 'spun_credits' ); ?>
 			<a href="http://www.researchstories.co.uk/" title="<?php esc_attr_e( 'We tell stories', 'spun' ); ?>" rel="generator"><?php printf( __( 'Copyright 2014 by Research Stories Ltd %s', 'spun' ), '' ); ?></a>
-			<span class="sep"> | </span>
+			<span class="sep"> </span>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon .site-footer -->
 </div><!-- #page .hfeed .site -->
